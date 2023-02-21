9e39c657eebce532f6058f6b9ef5bc86c17e6ec6
harshadO784494 <harshad.chavada@gmail.com>
2023-02-21 19:15:13 +0530

    adding to model branch

---
+++ data_processing.py
@@ -0,0 +1,11 @@
+
+
+print('add all the data used for fraud detection')
+
+a=15
+b=20
+def sum():
+    c= a+b
+    return c
+
+print(sum())
---
+++ ml_model.py
@@ -0,0 +1,12 @@
+a=15
+b=20
+def sum():
+    c= a+b
+    return c
+
+print(sum())
+
+def div():
+    e= a/b
+    return e
+print(div())
